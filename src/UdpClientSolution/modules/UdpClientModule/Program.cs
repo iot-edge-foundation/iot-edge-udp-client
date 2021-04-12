b8752f8109936ab659dd0507014a536dee3b3706
Sander van de Velde <sander@vandevelde-online.com>
2021-04-12 22:01:11 +0200

    ingest works

--- src/UdpClientSolution/modules/UdpClientModule/Program.cs
+++ src/UdpClientSolution/modules/UdpClientModule/Program.cs
@@ -10,10 +10,29 @@ namespace UdpClientModule
     using System.Threading.Tasks;
     using Microsoft.Azure.Devices.Client;
     using Microsoft.Azure.Devices.Client.Transport.Mqtt;
+    using Microsoft.Azure.Devices.Shared;
+    using Newtonsoft.Json;
+    using System.Net;
+    using System.Net.Sockets;
 
     class Program
     {
-        static int counter;
+        private static LogLevelMessage.LogLevel DefaultMinimalLogLevel = LogLevelMessage.LogLevel.Warning;
+
+        private static ModuleClient ioTHubModuleClient;
+
+        private const int DefaultClientListeningPort = 11001;
+
+        private static string _moduleId; 
+
+        private static string _deviceId;
+
+        private static UdpState _udpState;
+
+        private static LogLevelMessage.LogLevel MinimalLogLevel { get; set; } = DefaultMinimalLogLevel;
+
+        private static int ClientListeningPort { get; set; } = DefaultClientListeningPort;
+
 
         static void Main(string[] args)
         {
@@ -42,51 +61,234 @@ namespace UdpClientModule
         /// </summary>
         static async Task Init()
         {
+            _deviceId = System.Environment.GetEnvironmentVariable("IOTEDGE_DEVICEID");
+            _moduleId = Environment.GetEnvironmentVariable("IOTEDGE_MODULEID");
+
             MqttTransportSettings mqttSetting = new MqttTransportSettings(TransportType.Mqtt_Tcp_Only);
             ITransportSettings[] settings = { mqttSetting };
 
             // Open a connection to the Edge runtime
-            ModuleClient ioTHubModuleClient = await ModuleClient.CreateFromEnvironmentAsync(settings);
-            await ioTHubModuleClient.OpenAsync();
-            Console.WriteLine("IoT Hub module client initialized.");
+            ioTHubModuleClient = await ModuleClient.CreateFromEnvironmentAsync(settings);
+            
+            // Execute callback method for Twin desired properties updates
+            var twin = await ioTHubModuleClient.GetTwinAsync();
+            await onDesiredPropertiesUpdate(twin.Properties.Desired, ioTHubModuleClient);
 
-            // Register callback to be called when a message is received by the module
-            await ioTHubModuleClient.SetInputMessageHandlerAsync("input1", PipeMessage, ioTHubModuleClient);
+            Console.WriteLine("Supported desired properties: minimalLogLevel."); 
+
+
+            await ioTHubModuleClient.OpenAsync();
+
+            Console.WriteLine($"Module '{_deviceId}'-'{_moduleId}' initialized.");
+
+            var thread = new Thread(() => ThreadBody());
+            thread.Start();
         }
 
-        /// <summary>
-        /// This method is called whenever the module is sent a message from the EdgeHub. 
-        /// It just pipe the messages without any change.
-        /// It prints all the incoming messages.
-        /// </summary>
-        static async Task<MessageResponse> PipeMessage(Message message, object userContext)
-        {
-            int counterValue = Interlocked.Increment(ref counter);
 
-            var moduleClient = userContext as ModuleClient;
-            if (moduleClient == null)
+        private static async Task onDesiredPropertiesUpdate(TwinCollection desiredProperties, object userContext)
+        {
+            if (desiredProperties.Count == 0)
             {
-                throw new InvalidOperationException("UserContext doesn't contain " + "expected values");
+                Console.WriteLine("Empty desired properties ignored.");
+
+                return;
             }
 
-            byte[] messageBytes = message.GetBytes();
-            string messageString = Encoding.UTF8.GetString(messageBytes);
-            Console.WriteLine($"Received message: {counterValue}, Body: [{messageString}]");
-
-            if (!string.IsNullOrEmpty(messageString))
+            try
             {
-                using (var pipeMessage = new Message(messageBytes))
+                Console.WriteLine("Desired property change:");
+                Console.WriteLine(JsonConvert.SerializeObject(desiredProperties));
+
+                var client = userContext as ModuleClient;
+
+                if (client == null)
                 {
-                    foreach (var prop in message.Properties)
-                    {
-                        pipeMessage.Properties.Add(prop.Key, prop.Value);
-                    }
-                    await moduleClient.SendEventAsync("output1", pipeMessage);
+                    throw new InvalidOperationException($"UserContext doesn't contain expected ModuleClient");
+                }
+
+                var reportedProperties = new TwinCollection();
                 
-                    Console.WriteLine("Received message sent");
+                if (desiredProperties.Contains("clientListeningPort"))
+                {
+                    if (desiredProperties["clientListeningPort"] != null)
+                    {
+                        ClientListeningPort = Convert.ToInt32(desiredProperties["clientListeningPort"]);
+                    }
+                    else
+                    {
+                        ClientListeningPort = DefaultClientListeningPort;
+                    }
+
+                    Console.WriteLine($"ClientListeningPort changed to {ClientListeningPort}");
+
+                    reportedProperties["clientListeningPort"] = ClientListeningPort;
+                }
+
+                if (desiredProperties.Contains("minimalLogLevel"))
+                {
+                    if (desiredProperties["minimalLogLevel"] != null)
+                    {
+                        var minimalLogLevel = desiredProperties["minimalLogLevel"];
+
+                        // casting from int to enum needed
+                        var minimalLogLevelInteger = Convert.ToInt32(minimalLogLevel);
+
+                        MinimalLogLevel = (LogLevelMessage.LogLevel)minimalLogLevelInteger;
+                    }
+                    else
+                    {
+                        MinimalLogLevel = DefaultMinimalLogLevel;
+                    }
+
+                    Console.WriteLine($"MinimalLogLevel changed to '{MinimalLogLevel}'");
+
+                    reportedProperties["minimalLogLevel"] = MinimalLogLevel;
+                }
+                else
+                {
+                    Console.WriteLine($"MinimalLogLevel ignored");
+                }
+
+                if (reportedProperties.Count > 0)
+                {
+                    await client.UpdateReportedPropertiesAsync(reportedProperties).ConfigureAwait(false);
+
+                    Console.WriteLine("Changes to desired properties will be efficive on restarting the module.");
                 }
             }
-            return MessageResponse.Completed;
+            catch (AggregateException ex)
+            {
+                Console.WriteLine($"Desired properties change error: {ex.Message}");
+
+                var logLevelMessage = new LogLevelMessage { logLevel = LogLevelMessage.LogLevel.Error, code = "98", message = $"Desired properties change error: {ex.Message}" };
+
+                await SendLogLevelMessage(logLevelMessage);
+
+                foreach (Exception exception in ex.InnerExceptions)
+                {
+                    Console.WriteLine($"Error when receiving desired properties: {exception}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error when receiving desired properties: {ex.Message}");
+
+                var logLevelMessage = new LogLevelMessage { logLevel = LogLevelMessage.LogLevel.Error, code = "99", message = $"Error when receiving desired properties: {ex.Message}" };
+
+                await SendLogLevelMessage(logLevelMessage);
+            }
+        }
+
+        private static void ThreadBody()
+        {
+            // https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.udpclient.beginreceive?view=net-5.0
+
+            Console.WriteLine($"UDP Client listening on {ClientListeningPort}");
+
+            // IPEndPoint object will allow us to read datagrams sent from any source.
+            // The server knows us, we accept all servers
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, ClientListeningPort);
+            var udpClient = new UdpClient(RemoteIpEndPoint);
+
+            _udpState = new UdpState();
+            _udpState.e = RemoteIpEndPoint;
+            _udpState.u = udpClient;
+
+            Console.WriteLine("listening for first message");
+
+            try
+            {
+                while (true)
+                {
+                    // we have to repeat this non blocking call
+                    udpClient.BeginReceive(new AsyncCallback(ReceiveCallback), _udpState);
+                    //Console.WriteLine("non blocking call");
+                    Thread.Sleep(2000);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine($"Fatal ThreadBody exception: {ex.Message}");
+
+                var logLevelMessage = new LogLevelMessage { logLevel = LogLevelMessage.LogLevel.Critical, code = "00", message = $"ThreadBody exception: {ex.Message}" };
+
+                SendLogLevelMessage(logLevelMessage).Wait();
+
+                Console.WriteLine("Halted...");    
+            }
+            finally
+            {
+                udpClient.Close();
+                udpClient = null;
+            }
+        }
+
+        private static void ReceiveCallback(IAsyncResult ar)
+        {
+            UdpClient u = ((UdpState)(ar.AsyncState)).u;
+            IPEndPoint e = ((UdpState)(ar.AsyncState)).e;
+
+            byte[] receiveBytes = u.EndReceive(ar, ref e);
+            string receiveString = Encoding.ASCII.GetString(receiveBytes);
+
+            Console.WriteLine($"Received message: {receiveString} coming from server '{e.Address}:{e.Port}' ");
+
+            // simulate heavy logic running side-by-side async
+            //Thread.Sleep(10000);
+
+            Console.WriteLine($"Closed {receiveString}");
+        }
+
+        private static async Task SendLogLevelMessage(LogLevelMessage moduleStateMessage)
+        {
+            if (moduleStateMessage.logLevel < MinimalLogLevel)
+            {
+                return;
+            }
+
+            var jsonMessage = JsonConvert.SerializeObject(moduleStateMessage);
+
+            var messageBytes = Encoding.UTF8.GetBytes(jsonMessage);
+
+            using (var message = new Message(messageBytes))
+            {
+                message.ContentEncoding = "utf-8";
+                message.ContentType = "application/json";
+                message.Properties.Add("content-type", "application/opcua-error-json");
+
+                await ioTHubModuleClient.SendEventAsync("outputError", message);
+
+                var size = CalculateSize(messageBytes);
+
+                Console.WriteLine($"Error message {moduleStateMessage.code} with size {size} bytes sent.");
+            }
+        }
+
+        private static int CalculateSize(byte[] messageBytes)
+        {
+            using (var message = new Message(messageBytes))
+            {
+                message.ContentEncoding = "utf-8";
+                message.ContentType = "application/json";
+                message.Properties.Add("content-type", "application/opcua-error-json"); // not flexible
+
+                var result = message.GetBytes().Length;
+
+                foreach (var p in message.Properties)
+                {
+                    result = result + p.Key.Length + p.Value.Length;
+                }
+
+                return result;
+            }
         }
     }
+
+        public struct UdpState
+        {
+            public UdpClient u;
+            public IPEndPoint e;
+        }
+
 }
